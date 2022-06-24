beaef27716a13223e3c2157a6d07cdb773e5b5e6
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-06-24 20:08:07 -0300

    Create ViewSet

--- Django3/escola/views.py
+++ Django3/escola/views.py
@@ -1,8 +1,15 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
 
 
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunos"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
-def alunos (request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome': 'Leonardo'}
-        return JsonResponse(aluno)
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
