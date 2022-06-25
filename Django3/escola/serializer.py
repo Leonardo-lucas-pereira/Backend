9ac69a4aa827bf53bb604c62e410c33698b1d626
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-06-24 21:57:42 -0300

    Add Matricula

--- Django3/escola/serializer.py
+++ Django3/escola/serializer.py
@@ -1,6 +1,6 @@
 from pyexpat import model
 from rest_framework import serializers
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 
 class AlunoSerializer(serializers.ModelSerializer):
@@ -12,3 +12,9 @@ class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
+
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
