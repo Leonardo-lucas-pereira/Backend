f68c0cfcb5faad4a8d8be6656a57bc97e3c0f5b6
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-06-26 21:17:28 -0300

    New Project: Django Clintes

---
+++ Django_clientes/clientes/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+    def validade_cpf(self, cpf):
+        print(f"{cpf} size {len(cpf)}")
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
+        return cpf
+
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("O nome deve ser em string")
+        return nome
+
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve ter 9 dígitos")
+        return rg
+
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+             raise serializers.ValidationError("O celular deve ter 11 dígitos")
+        return celular
+
+
+    
