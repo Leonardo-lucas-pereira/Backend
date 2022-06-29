377e583462092af073d95a83ed983ee8cce87519
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-06-28 21:51:07 -0300

    Validação de celular e cpf

--- Django_clientes/clientes/serializers.py
+++ Django_clientes/clientes/serializers.py
@@ -15,6 +15,5 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError({'celular':"O celular deve ter 11 dígitos"})
+            raise serializers.ValidationError({'celular':"O número de celular deve ser o modelo: 99 99999-9999"})
         return data
-  
--- Django_clientes/clientes/validators.py
+++ Django_clientes/clientes/validators.py
@@ -1,5 +1,9 @@
+import re
+from validate_docbr import CPF
+
 def cpf_valido(numero_cpf):
-    return len(numero_cpf) == 11
+    cpf = CPF()
+    return cpf.validate(numero_cpf)
 
 def nome_valido( nome):
     return nome.isalpha()
@@ -8,5 +12,8 @@ def rg_valido(numero_rg):
     return len(numero_rg) == 9
 
 def celular_valido(numero_celular):
-    return len(numero_celular) == 11
+    """Verifica se o celular é valido"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, numero_celular)
+    return resposta
     
